df11a7e2db1832bf2ee1ef7f14424fa266efd194
Mohidul Abedin <mabedin00@stuy.edu>
2019-03-24 15:55:57 -0400

    basic completion

--- List_inChainOfNodes.java
+++ List_inChainOfNodes.java
@@ -4,34 +4,56 @@
 
 public class List_inChainOfNodes{
     private Node headReference;
-
     /**
       Construct an empty list
      */
-     
+
+    public List_inChainOfNodes(){
+    }
+
 
     /**
       @return the number of elements in this list
      */
-    // public int size() {
-    // }
+    public int size() {
+        int output = 0;
+        Node currentNode = new Node(headReference);
+        currentNode = headReference;
+        while (currentNode != null){
+            currentNode = currentNode.getReferenceToNextNode();
+            output++;
+        }
+        return output;
+    }
 
-    
+
      /**
        @return a string representation of this list,
        format:
-           # elements [element0,element1,element2,] 
+           # elements [element0,element1,element2,]
       */
-    // public String toString() {
-    // }
-    
-    
+    public String toString() {
+        String output = "[";
+        int counter = 0;
+        Node currentNode = headReference;
+        while (counter < size()){
+            output += currentNode.getCargoReference();
+            counter++;
+            currentNode = currentNode.getReferenceToNextNode();
+        }
+        return output + "]";
+    }
+
+
     /**
       Append @value to the head of this list.
 
       @return true, in keeping with conventions yet to be discussed
      */
      public boolean addAsHead( Object val) {
+         Node temp = headReference;
+         headReference = new Node(val, temp);
+
         return true;
      }
 }
